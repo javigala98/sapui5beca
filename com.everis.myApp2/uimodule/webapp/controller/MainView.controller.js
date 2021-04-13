15a6cb5de6d4ffc72a8d033fb59b6b7b5afd0383
jgalaber <jorge.barrera.villau@everis.com>
2021-04-13 16:56:31 +0200

    Comentarios añadidos del funcionamiento del programa myApp2

--- com.everis.myApp2/uimodule/webapp/controller/MainView.controller.js
+++ com.everis.myApp2/uimodule/webapp/controller/MainView.controller.js
@@ -4,24 +4,32 @@ sap.ui.define([
 ], function (Controller, JSONModel) {
     "use strict";
 
+    // Definición de variables globales
     var oView, oController;
 
     return Controller.extend("com.everis.myApp.controller.MainView", {
 
+      // Seteamos las variables globales para contener la vista y el controlador de la vista
       onInit: function() {
         oView       = this.getView();
         oController = this.getView().getController();
       },
 
+      // Despues de que haya cargado la web, se ejecuta la llamada a la función 'setDataFromNorthwindApiToTableModel'
       onAfterRendering: function() {
         oController.setDataFromNorthwindApiToTableModel();
       },
 
+      /**
+       *  Obtenemos de la vista el modelo 'oModelNorthwind' el cual contiene el oModelData,
+       *    se realiza la llamada a la función, y se setea la respuesta al modelo 'NorthWindData',
+       *    en formato JSONModel, el cual esta bindeado al contenido de la tabla.
+       */
       setDataFromNorthwindApiToTableModel: function() {
 
-        oView.getModel('oModelNorthwind').read('/Orders', {
+        oView.getModel("oModelNorthwind").read("/Orders", {
           success: function (response) {
-            oView.setModel( new JSONModel(response), 'NorthWindData' );
+            oView.setModel( new JSONModel(response), "NorthWindData" );
           },
           error: function (error) {
             // eslint-disable-next-line no-console
